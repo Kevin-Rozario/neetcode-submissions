c6ad712a7bdad13d36f150723e886dd043bf8728
Kevin Rozario <91315009+Kevin-Rozario@users.noreply.github.com>
2026-04-23 17:04:42 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.cpp
@@ -0,0 +1,26 @@
+class Solution {
+   public:
+    string encode(vector<string>& strs) {
+        string encoded = "";
+        for (const string& s : strs) {
+            encoded += to_string(s.size()) + "#" + s;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> result;
+        int i = 0;
+        while (i < s.size()) {
+            int j = i;
+            while (s[j] != '#') j++;
+
+            int length = stoi(s.substr(i, j - i));
+            i = j + 1;  // Move past '#'
+
+            result.push_back(s.substr(i, length));
+            i += length;
+        }
+        return result;
+    }
+};
