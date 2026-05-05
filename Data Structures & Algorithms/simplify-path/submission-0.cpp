89950bc8c4f7a7a92caf29f98736cd1f38d7a46d
Kevin Rozario <91315009+Kevin-Rozario@users.noreply.github.com>
2026-05-05 17:28:16 +0530

    Add: simplify-path - submission-0

---
+++ Data Structures & Algorithms/simplify-path/submission-0.cpp
@@ -0,0 +1,30 @@
+class Solution {
+   public:
+    string simplifyPath(string path) {
+        vector<string> st;
+        int n = path.size();
+
+        for (int i = 0; i < n; i++) {
+            if (path[i] == '/') continue;
+
+            string temp = "";
+            while (i < n && path[i] != '/') {
+                temp += path[i];
+                i++;
+            }
+
+            if (temp == ".")
+                continue;
+            else if (temp == "..") {
+                if (!st.empty()) st.pop_back();
+            } else {
+                st.push_back(temp);
+            }
+        }
+
+        string res = "";
+        for (string s : st) res += "/" + s;
+
+        return res.empty() ? "/" : res;
+    }
+};
